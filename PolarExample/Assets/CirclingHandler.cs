e971a2f09cbf0d5025862ad6990abcba2fade129
LunaLovecraft <lunalovecraft@gmail.com>
2015-10-06 10:12:28 -0400

    Added in circle detection via linecasting for maximal accuracy.

---
+++ PolarExample/Assets/CirclingHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CirclingHandler : MonoBehaviour {
+
+    public bool isCircled = false;
+
+    void Update()
+    {
+        if (isCircled)
+        {
+            onCircle();
+            isCircled = false;
+        }
+    }
+
+    private void onCircle()
+    {
+        Debug.Log("I am circled.");
+    }
+
+
+
+
+}
--- PolarExample/Assets/TrailManager.cs
+++ PolarExample/Assets/TrailManager.cs
@@ -38,6 +38,8 @@ public class TrailManager : MonoBehaviour {
     int oldPoint = 0;
     int nextPoint = 1;
 
+    RaycastHit2D[] hit;
+
     public GameObject myParticleSystem;
     
 	// Use this for initialization
@@ -45,6 +47,7 @@ public class TrailManager : MonoBehaviour {
         prevPos = this.transform.position;
         this.GetComponent<TrailRenderer>().time = 1.5f;
         myPoints[0] = new Point((Vector2)prevPos);
+        hit = new RaycastHit2D[1];
 	}
 
     // FixedUpdate is called at a fixed interval.
@@ -86,14 +89,14 @@ public class TrailManager : MonoBehaviour {
             if (possibleIntersects.Count > 0)
             {
 
-                for (int i = 0; i < possibleIntersects.Count; i++)
+                for (int i = 0; i < possibleIntersects.Count; ++i)
                 {
 
                     IntersectIndex myIntersect = (IntersectIndex)possibleIntersects[i];
                     float minX = myPoints[myIntersect.x].pos.x, minY = myPoints[myIntersect.x].pos.y,
                         maxX = myPoints[myIntersect.x].pos.x, maxY = myPoints[myIntersect.x].pos.y;
 
-                    for (int x = myIntersect.x; x < myIntersect.z; x++)
+                    for (int x = myIntersect.x; x < myIntersect.z; ++x)
                     {
 
                         if (myPoints[x].pos.x < minX)
@@ -108,9 +111,25 @@ public class TrailManager : MonoBehaviour {
 
                     if (maxX - minX > 1.0f && maxY - minY > 1.0f)
                     {
-                        Debug.Log("Intersection " + i + " succeeded");
+                        
                         Debug.DrawLine(myPoints[myIntersect.x].pos, myPoints[myIntersect.y].pos, Color.red);
                         Instantiate(myParticleSystem, new Vector2(myPoints[myIntersect.x].pos.x, myPoints[myIntersect.x].pos.y), Quaternion.identity);
+
+                        for (int j = 1; j < myIntersect.w - myIntersect.x; ++j)
+                        {
+                            int mask = 1 << 8;
+                            Physics2D.LinecastNonAlloc(myPoints[myIntersect.x].pos, myPoints[myIntersect.x + j].pos, hit, mask);
+                            Debug.DrawLine(myPoints[myIntersect.x].pos, myPoints[myIntersect.x + j].pos);
+                            for (int k = 0; k < hit.Length; ++k)
+                            {
+                                if (!hit[k])
+                                {
+                                    break;
+                                }
+                                hit[k].collider.gameObject.GetComponent<CirclingHandler>().isCircled = true;
+                            }
+                            hit = new RaycastHit2D[1];
+                        }
                     }
                 }
             }
